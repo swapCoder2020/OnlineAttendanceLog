dc59bd986febc1fac9a9f87cbdda8db99b072f36
chinmay-r-gharat <chinmay1992.cg@gmail.com>
2020-03-25 10:44:30 +0530

    Registration app created and registration.html saved in templates folder and linked to 0:8000/registration/ path, also the css file is loaded statically.

--- onlineattendance/onlineattendance/urls.py
+++ onlineattendance/onlineattendance/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mainpage.urls'))
+    path('', include('mainpage.urls')),
+    path('registration/', include('registrationpage.urls'))
 ]
+
