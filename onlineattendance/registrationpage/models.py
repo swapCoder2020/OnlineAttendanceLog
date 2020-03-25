580c412928691554b5a73fb7c3f241fa24407114
chinmay-r-gharat <chinmay1992.cg@gmail.com>
2020-03-25 23:26:08 +0530

    Model for registration page created and a simple user registered directly using **kwargs.

--- onlineattendance/registrationpage/models.py
+++ onlineattendance/registrationpage/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=20, blank=False)
+    middle_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=20, blank=True)
+    contact = models.IntegerField(blank=False)
+    address = models.CharField(max_length=300, blank=False)
+    username = models.CharField(max_length=20, blank=False, unique=True)
+    email = models.EmailField(blank=False, unique=True)
+    password = models.CharField(max_length=30, blank=False)
+
+    def __str__(self):
+        return "{} {} {}".format(Users.first_name, Users.last_name, Users.email)
--- onlineattendance/registrationpage/views.py
+++ onlineattendance/registrationpage/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Users
 
 def registrationPage(request):
     if request.method == "POST":
-        print(request.POST)
+        temp = Users(**{'first_name':'chinmay', 'middle_name':'rajan', 'last_name':'gharat'
+                        ,'contact':7507511126, 'address':'avasasda asdad', 'username':'chinu', 'email':'c@c.com', 'password':'qwerty1234'})
+        temp.save()
     return render(request, 'registration.html')
