cfbb6a7684288105d9900005f4eee8b76e1c2466
chinmay-r-gharat <chinmay1992.cg@gmail.com>
2020-03-25 22:06:43 +0530

    Instead of rendering the registration.html and login.html pages they were redirected to it's registrationPage and loginPage views.

--- onlineattendance/mainpage/views.py
+++ onlineattendance/mainpage/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def mainPage(request):
     if request.method == 'GET':
         try:
             if request.GET['Register']:
-                return render(request, 'registration.html')
+                return redirect('registrationPage')
         except:
             pass
         try:
             if request.GET['Login']:
-                return render(request, 'login.html')
+                return redirect('loginPage')
         except:
             pass
         return render(request, 'main.html')
--- onlineattendance/registrationpage/views.py
+++ onlineattendance/registrationpage/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def registrationPage(request):
+    if request.method == "POST":
+        print(request.POST)
     return render(request, 'registration.html')
