fa4f8def4eeeeb2f4abae0469443ff6c3aa1382e
chinmay-r-gharat <chinmay1992.cg@gmail.com>
2020-03-29 19:24:18 +0530

    Post method is used to pass the data from login form and once the data is received user is redirected to main page.

--- onlineattendance/loginpage/views.py
+++ onlineattendance/loginpage/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def loginPage(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return redirect('/')
     return render(request, 'login.html')
